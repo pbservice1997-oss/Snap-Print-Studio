5cd5ceaf390fd763b47e251ff1bcdee8fc223dd9
pbservice1997-oss <pbservice1997@gmail.com>
2026-02-28 22:51:21 +0700

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'snap-studio-v2';
+const CACHE_NAME = 'snap-studio-v-3';
 const ASSETS = [
   '/',
   '/index.html',
@@ -21,3 +21,4 @@ self.addEventListener('fetch', (e) => {
   );
 
 });
+
